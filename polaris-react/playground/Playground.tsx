a2fafbce75c7277368a055a0f11ce7de0b1e93b9
Reuben Dayo <reuben.dayo@shopify.com>
2023-02-17 05:08:00 +0000

    remove playground

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
 
-import {KeyboardKey, Page} from '../src';
+import {Page} from '../src';
 
 export function Playground() {
   return (
-    <div style={{background: '#548365', height: '100%', padding: '100px'}}>
-      <Page title="Playground">
-        <div>
-          <KeyboardKey>⌘</KeyboardKey>
-          <KeyboardKey>Q</KeyboardKey>
-          <KeyboardKey>W</KeyboardKey>
-          <KeyboardKey>E</KeyboardKey>
-          <KeyboardKey>R</KeyboardKey>
-        </div>
-        <div>
-          <KeyboardKey variant="small">⌘</KeyboardKey>
-          <KeyboardKey variant="small">Q</KeyboardKey>
-          <KeyboardKey variant="small">W</KeyboardKey>
-          <KeyboardKey variant="small">E</KeyboardKey>
-          <KeyboardKey variant="small">R</KeyboardKey>
-        </div>
-      </Page>
-    </div>
+    <Page title="Playground">
+      {/* Add the code you want to test in here */}
+    </Page>
   );
 }
