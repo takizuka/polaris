89b439cd2a27f4181443956f8ee1d2440066a424
Alex Page <hi@alexpage.dev>
2023-02-17 18:36:00 +1100

    Replace convertProps with convertProps2
    Co-Authored-By: Marten Bjork <marten@martenbjork.com>

--- polaris-studio/scripts/convertProps.ts
+++ polaris-studio/scripts/convertProps.ts
@@ -1,17 +1,19 @@
 import fs from "fs";
 import { createElement } from "react";
-import untypedPolarisTpyes from "../polarisProps.json";
-import { ComponentMap, PropDefinition, PropType, PropValue } from "../types";
-const types = untypedPolarisTpyes as AllTypes;
+import untypedPolarisTypes from "../polarisProps.json";
+import {
+  ComponentMap,
+  GroupPropDefinition,
+  PropDefinition,
+  PropType,
+  PropValue,
+} from "../types";
+const types = untypedPolarisTypes as AllTypes;
 
 export type AllTypes = {
   [typeName: string]: {
     [filePath: string]: Type;
   };
-};
-
-export type FilteredTypes = {
-  [typeName: string]: Type;
 };
 
 export type Type = {
@@ -26,23 +28,54 @@ export type Type = {
   members?: Type[];
 };
 
-let components: ComponentMap = {};
+const NO_DESCRIPTION = "No description";
 
-function typeToProps(
-  type: Type,
-  originalName: string
-): { [key: string]: PropDefinition } {
-  let props: { [key: string]: PropDefinition } = {};
+const getTypeByKey = (key: string, currentFileName?: string): Type | null => {
+  const match = types[key];
+  if (match) {
+    const fileOccurences = Object.keys(match).length;
+    if (fileOccurences > 1) {
+      if (currentFileName && match[currentFileName]) {
+        return match[currentFileName];
+      } else {
+        return null;
+      }
+    } else if (fileOccurences === 1) {
+      return match[Object.keys(match)[0]];
+    }
+  }
+  return null;
+};
 
-  if (type.name === "BorderTokenAlias") {
-    console.log({ log: "WOOOOOO, BORDER_TOKEN_ALIAS" });
+const createPropDefinition = (
+  key: string,
+  memberName?: string
+): PropDefinition | null => {
+  let type = getTypeByKey(key);
+  if (!type) return null;
+  if (memberName) {
+    if (!type.members) return null;
+    const matchingMember = type.members.find(
+      (member) => member.name === memberName
+    );
+    if (!matchingMember) return null;
+    type = matchingMember;
   }
-  if (type.name === "BannerStatus") {
-    console.log("🚀🚀🚀🚀🚀", type);
-  }
-  switch (type.syntaxKind) {
-    case "MethodSignature":
-      props[type.name] = {
+  const value = type.value.toString();
+
+  if (type.syntaxKind) {
+    const pascalCaseRegex = /^[A-Z][a-z]+(?:[A-Z][a-z]+)*$/g;
+
+    if (value.match(pascalCaseRegex)) {
+      const resolvedType = getTypeByKey(value, type.filePath);
+      if (resolvedType) {
+        const propDefinition = createPropDefinition(resolvedType.name);
+        return propDefinition;
+      }
+    }
+
+    if (type.syntaxKind === "MethodSignature") {
+      return {
         type: PropType.Action,
         defaultValue: {
           type: PropType.Action,
@@ -50,185 +83,157 @@ function typeToProps(
         },
         description: type.description || "No description",
       };
+    }
 
-      break;
-
-    case "TypeAliasDeclaration":
-    case "PropertySignature":
-      if (type.value.toString().toLowerCase() === "string") {
-        props[type.name] = {
+    if (
+      value === "string" ||
+      value === "string[]" ||
+      value === "string | string[]"
+    ) {
+      return {
+        type: PropType.String,
+        defaultValue: {
           type: PropType.String,
-          defaultValue: {
-            type: PropType.String,
-            value: "",
-          },
-          description: type.description || "No description",
-        };
-      } else if (type.value === "boolean") {
-        props[type.name] = {
+          value: "",
+        },
+        description: type.description || NO_DESCRIPTION,
+      };
+    } else if (value === "boolean") {
+      return {
+        type: PropType.Boolean,
+        defaultValue: {
           type: PropType.Boolean,
-          defaultValue: {
-            type: PropType.Boolean,
-            value: type.defaultValue === "true" ? true : false,
-          },
-          description: type.description || "No description",
-        };
-      } else if (type.value === "number") {
-        props[type.name] = {
+          value: false,
+        },
+        description: type.description || NO_DESCRIPTION,
+      };
+    } else if (value === "number") {
+      return {
+        type: PropType.Number,
+        defaultValue: {
           type: PropType.Number,
+          value: 0,
+        },
+        description: type.description || NO_DESCRIPTION,
+      };
+    } else if (
+      type.value.toString() === "React.ReactNode" ||
+      type.value.toString() === "ReactNode" ||
+      type.value.toString() === "React.ReactElement"
+    ) {
+      return {
+        type: PropType.ReactNode,
+        defaultValue: {
+          type: PropType.ReactNode,
+        },
+        description: type.description || "No description",
+      };
+    } else if (type.value.toString().includes("|")) {
+      const parts = value.split("|").map((part) => part.trim());
+      let allStrings = true;
+      parts.forEach((part) => {
+        const isString = part.replace(/'/g, '"').match(/^"[^"]+"$/gi) !== null;
+        if (!isString) allStrings = false;
+      });
+
+      if (allStrings) {
+        const options = parts.map((part) =>
+          part.trim().replace(/"/g, "").replace(/'/g, "")
+        );
+        return {
+          type: PropType.Enum,
+          options,
           defaultValue: {
-            type: PropType.Number,
-            value: type.defaultValue ? parseInt(type.defaultValue) : 0,
+            type: PropType.Enum,
+            value: options[0],
           },
           description: type.description || "No description",
         };
-      } else if (type.value.toString().includes("|")) {
-        const parts = type.value
-          .toString()
-          .split("|")
-          .map((part) => part.trim());
-        let allStrings = true;
+      } else {
+        // Other enums that we can't parse
+      }
+    } else if (value.includes("&")) {
+      // We can handle simple unions like "Foo & Bar" where
+      // both of them have members. In that case, we mash
+      // their props together
+      const parts = value.split("&").map((part) => part.trim());
+      let allHaveMembers = true;
+      parts.forEach((part) => {
+        const type = getTypeByKey(part);
+        if (!type || !type.members) allHaveMembers = false;
+      });
+
+      if (allHaveMembers) {
+        let propDefinition: GroupPropDefinition = {
+          type: PropType.Group,
+          defaultValue: {
+            type: PropType.Group,
+            children: {},
+          },
+          description: type.description || NO_DESCRIPTION,
+          children: {},
+        };
         parts.forEach((part) => {
-          const isString =
-            part.replace(/'/g, '"').match(/^"[^"]+"$/gi) !== null;
-          if (!isString) {
-            allStrings = false;
+          const type = getTypeByKey(part);
+          if (type) {
+            type.members?.forEach((member) => {
+              if (type) {
+                const memberDefinition = createPropDefinition(
+                  type.name,
+                  member.name
+                );
+                if (memberDefinition) {
+                  propDefinition.children = {
+                    ...propDefinition.children,
+                    [member.name]: memberDefinition,
+                  };
+                }
+              }
+            });
           }
         });
-        if (type.name === "BannerStatus") {
-          console.log(
-            "🚀🚀🚀🚀🚀🤪",
-            parts,
-            allStrings,
-            type.name,
-            originalName
-          );
-        }
-
-        if (allStrings) {
-          const options = parts.map((part) =>
-            part.trim().replace(/"/g, "").replace(/'/g, "")
-          );
-          props[originalName] = {
-            type: PropType.Enum,
-            options,
-            defaultValue: {
-              type: PropType.Enum,
-              value: options[0],
-            },
-            description: type.description || "No description",
-          };
-        } else {
-          //   console.log({
-          //     log: "unparsed enum",
-          //     name: member.name,
-          //     syntaxKind: member.syntaxKind,
-          //     value: member.value.toString(),
-          //   });
-        }
-      } else if (
-        type.value.toString() === "React.ReactNode" ||
-        type.value.toString() === "ReactNode" ||
-        type.value.toString() === "React.ReactElement"
-      ) {
-        props[type.name] = {
-          type: PropType.ReactNode,
-          defaultValue: {
-            type: PropType.ReactNode,
-          },
-          description: type.description || "No description",
-        };
-      } else if (
-        type.value.toString()[0].match(/^[A-Z]/) &&
-        type.value.toString().match(/[a-z]$/gi)
-      ) {
-        const subProps = extractProps(type.value.toString(), type.filePath);
-
-        if (subProps.didHaveMembers) {
-          props[type.name] = {
-            type: PropType.Group,
-            children: {
-              ...subProps.props,
-            },
-            defaultValue: {
-              type: PropType.Group,
-              children: {},
-            },
-            description: type.description || "No description",
-          };
-        } else {
-          Object.entries(subProps.props).forEach(([subPropName, subProp]) => {
-            props[subPropName] = subProp;
-          });
-        }
-      } else {
-        // console.log({
-        //   log: "unparsed type",
-        //   name: type.name,
-        //   syntaxKind: type.syntaxKind,
-        //   value: type.value.toString(),
-        // });
+        return propDefinition;
       }
-  }
-  return props;
-}
-
-function extractProps(
-  typeName: string,
-  currentFileName?: string
-): {
-  props: { [key: string]: PropDefinition };
-  didHaveMembers: boolean;
-} {
-  let props: { [key: string]: PropDefinition } = {};
-  let didHaveMembers = false;
-
-  if (!types[typeName]) {
-    console.log({ log: "No definitions found for type" });
-    return { props: {}, didHaveMembers: false };
-  }
-
-  const hasManyDefinitions = Object.keys(types[typeName]).length > 1;
-  if (hasManyDefinitions) {
-    if (currentFileName && !types[typeName][currentFileName]) {
-      console.log({ log: "Multiple files found for type" });
-      return { props: {}, didHaveMembers: false };
-    } else {
-      // !
+    }
+  } else {
+    if (type.members) {
+      let propDefinition: GroupPropDefinition = {
+        type: PropType.Group,
+        defaultValue: {
+          type: PropType.Group,
+          children: {},
+        },
+        description: type.description || NO_DESCRIPTION,
+        children: {},
+      };
+      type.members.forEach((member) => {
+        if (type) {
+          const memberDefinition = createPropDefinition(type.name, member.name);
+          if (memberDefinition) {
+            propDefinition.children = {
+              ...propDefinition.children,
+              [member.name]: memberDefinition,
+            };
+          }
+        }
+      });
+      return propDefinition;
     }
   }
 
-  const type = Object.values(types[typeName])[0];
-  if (!type) throw new Error(`Could not find type ${typeName}`);
+  return null;
+};
 
-  if (type.syntaxKind === "TypeAliasDeclaration") {
-    props = { ...props, ...typeToProps(type, type.name) };
-  }
+let result: any = {};
 
-  if (type.members) {
-    didHaveMembers = true;
-    type.members.forEach((member) => {
-      props = { ...props, ...typeToProps(member, typeName) };
-    });
-  }
-
-  return { props, didHaveMembers };
-}
-
-Object.entries(types).forEach(([typeName]) => {
-  if (typeName.endsWith("Props")) {
-    const componentName = typeName.replace("Props", "");
-    components[componentName] = {
-      props: {},
-      allowedChildren: "",
-      reactComponent: () => `createElement("div", {}, [${componentName}])`,
-      renderPreview: () =>
-        createElement("div", {}, [`${componentName} preview`]),
-    };
-    components[componentName].props = extractProps(typeName).props;
+Object.keys(types).forEach((key) => {
+  const isPropDefinition = key.endsWith("Props");
+  if (isPropDefinition) {
+    const propDefinition = createPropDefinition(key);
+    if (propDefinition && propDefinition.type === PropType.Group) {
+      result[key] = propDefinition.children;
+    }
   }
 });
 
-// console.log(components);
-
-fs.writeFileSync("./components.json", JSON.stringify(components, null, 2));
+fs.writeFileSync("./componentProps.json", JSON.stringify(result, null, 2));
