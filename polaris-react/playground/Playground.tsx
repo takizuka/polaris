0e6c00df075a0e5006e7b643712edb690b60ba29
Reuben Dayo <reuben.dayo@shopify.com>
2023-02-17 04:43:41 +0000

    update playground

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
-import {Page} from '../src';
+import {KeyboardKey, Page} from '../src';
 
 export function Playground() {
   return (
-    <Page title="Playground">
-      {/* Add the code you want to test in here */}
-    </Page>
+    <div style={{background: '#548365', height: '100%', padding: '100px'}}>
+      <Page title="Playground">
+        <div>
+          <KeyboardKey>⌘</KeyboardKey>
+          <KeyboardKey>Q</KeyboardKey>
+          <KeyboardKey>W</KeyboardKey>
+          <KeyboardKey>E</KeyboardKey>
+          <KeyboardKey>R</KeyboardKey>
+        </div>
+        <div>
+          <KeyboardKey variant="small">⌘</KeyboardKey>
+          <KeyboardKey variant="small">Q</KeyboardKey>
+          <KeyboardKey variant="small">W</KeyboardKey>
+          <KeyboardKey variant="small">E</KeyboardKey>
+          <KeyboardKey variant="small">R</KeyboardKey>
+        </div>
+      </Page>
+    </div>
   );
 }
